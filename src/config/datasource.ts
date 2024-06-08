87b3eef0d7ef85e0ed9bc18c54f2c9353e9f1da7
Christopher Bryan Gallego Gomez <christopher.gallego@ant.gov.co>
2024-06-08 07:17:27 -0500

    Moved to connection with backend architecture

--- src/config/datasource.ts
+++
@@ -1,9 +0,0 @@
-import Dexie from 'dexie';
-
-const datasource = new Dexie('mydb');
-
-datasource.version(1).stores({
-    coordinates: '++id, latitude, longitude, captured_at' // Primary key and indexed fields
-});
-
-export default datasource;
--- src/db/model.ts
+++ src/db/model.ts
@@ -1,14 +1,24 @@
-import datasource from "../config/datasource";
-import { Coordinate } from "../types/coordinates";
-
 export const addCoordinate = async (latitude: number, longitude: number) => {
-  await datasource.table<Coordinate>('coordinates').add({
-    latitude,
-    longitude,
-    captured_at: new Date()
+  const response = await fetch('http://localhost:4000/coordinates', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      latitude,
+      longitude
+    })
   });
+
+  if (!response.ok) {
+    throw new Error('Failed to add coordinate');
+  }
 };
 
 export const getCoordinates = async () => {
-  return await datasource.table<Coordinate>('coordinates').toArray();
+  const response = await fetch('http://localhost:4000/coordinates');
+  if (!response.ok) {
+    throw new Error('Failed to fetch coordinates');
+  }
+  return await response.json();
 };
