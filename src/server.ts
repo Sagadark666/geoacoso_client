982e2820e0e559bc1dd6506e180958dc9ea33a2f
Christopher Bryan Gallego Gomez <christopher.gallego@ant.gov.co>
2024-06-08 07:56:04 -0500

    Serving fix

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the dist directory
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// Serve index.html on all requests
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
